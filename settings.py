d84cce2d14845c7a63fbd23a5343e06e10e7bd55
Artem Kulikov <chigar2010@yandex.ru>
2024-06-12 18:32:37 +0200

    deploy

--- settings.py
+++ settings.py
@@ -15,12 +15,6 @@ DB_PASSWORD = os.getenv('POSTGRES_PASSWORD')
 DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
 
-DB_NAME = os.getenv('POSTGRES_DB')
-DB_USER = os.getenv('POSTGRES_USER')
-DB_PASSWORD = os.getenv('POSTGRES_PASSWORD')
-DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT')
-
 
 class Config(object):
     SQLALCHEMY_DATABASE_URI = (
