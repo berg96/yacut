e4bef981bde280425a2ee1fa0bc67e0f8e4cbfeb
Artem Kulikov <chigar2010@yandex.ru>
2024-06-12 18:32:37 +0200

    deploy

--- settings.py
+++ settings.py
@@ -9,7 +9,16 @@ SHORT_PATTERN = (
     f'^[{re.escape(SHORT_CHARACTERS)}]+$'
 )
 
+DB_NAME = os.getenv('POSTGRES_DB')
+DB_USER = os.getenv('POSTGRES_USER')
+DB_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = (
+        f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+    )
+    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SECRET_KEY = os.getenv('SECRET_KEY')
