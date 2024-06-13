d78a6bd01e550bbca6bdbe287a1bc586c35d783e
Artem Kulikov <chigar2010@yandex.ru>
2024-06-13 12:47:49 +0200

    last last fix

--- yacut/views.py
+++ yacut/views.py
@@ -11,8 +11,7 @@ def index_view():
     form = URLMapForm()
     if not form.validate_on_submit():
         return render_template('index.html', form=form)
-    short_from_user = form.custom_id.data
-    short = URLMap.create(form.original_link.data, short_from_user).short
+    short = URLMap.create(form.original_link.data, form.custom_id.data).short
     short_url = f'{url_for(REDIRECT_FUNC_NAME, _external=True, short=short)}'
     return render_template('index.html', form=form, short_url=short_url)
 
