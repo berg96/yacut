dbe0e87e20972821fc05eaed71e00a88f1f9c002
Artem Kulikov <chigar2010@yandex.ru>
2024-06-13 20:17:27 +0200

    update env_example and settings

--- settings.py
+++ settings.py
@@ -8,13 +8,6 @@ SHORT_CHARACTERS = string.ascii_letters + string.digits
 SHORT_PATTERN = (
     f'^[{re.escape(SHORT_CHARACTERS)}]+$'
 )
-
-DB_NAME = os.getenv('POSTGRES_DB')
-DB_USER = os.getenv('POSTGRES_USER')
-DB_PASSWORD = os.getenv('POSTGRES_PASSWORD')
-DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT')
-
 DB_NAME = os.getenv('POSTGRES_DB')
 DB_USER = os.getenv('POSTGRES_USER')
 DB_PASSWORD = os.getenv('POSTGRES_PASSWORD')
@@ -26,5 +19,4 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = (
         f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
     )
-    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SECRET_KEY = os.getenv('SECRET_KEY')
